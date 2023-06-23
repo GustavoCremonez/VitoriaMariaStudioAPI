a0532e31d5530ec0eb98ba4725dbf2fb0937f873
GustavoCremonez <aparecido2332001@gmail.com>
2023-06-23 15:35:56 -0300

    Adding context and configuring it in the API with connection to SQL Server

---
+++ VitoriaMariaStudio/VitoriaMariaStudio.Repository/Context/StudioDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VitoriaMariaStudio.Repository.Context
+{
+    public class StudioDbContext : DbContext
+    {
+        public StudioDbContext(DbContextOptions<StudioDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<entity> table
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // map of entitys here
+        }
+    }
+}
