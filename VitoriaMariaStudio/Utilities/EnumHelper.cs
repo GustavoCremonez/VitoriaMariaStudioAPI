b3520f6b45ed86c83f82b34f6f368bb256bca142
GustavoCremonez <aparecido2332001@gmail.com>
2023-06-23 17:40:18 -0300

    Adding project for shared/helper classes to all projects in the solution and adding classes to help with enums

---
+++ VitoriaMariaStudio/Utilities/EnumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+public static class EnumHelper
+{
+    public static string GetEnumDescription(Enum value)
+    {
+        FieldInfo fieldInfo = value.GetType().GetField(value.ToString());
+        DescriptionAttribute[] attributes = (DescriptionAttribute[])fieldInfo.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+        if (attributes.Length > 0)
+            return attributes[0].Description;
+
+        return value.ToString();
+    }
+}
